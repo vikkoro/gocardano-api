08d154bf0dabed4028e75f296d34bcfcb2c50ef5
viktorkoro <viktor.koro@grassbusinesslabs.com>
2021-11-08 16:15:48 +0200

    added handler module

---
+++ handler/main.go
@@ -0,0 +1,124 @@
+package handlers
+
+import (
+	//"encoding/json"
+	"fmt"
+	//"github.com/ethereum/go-ethereum/ethclient"
+	"net/http"
+
+	//Models "github.com/LuisAcerv/goeth-api/models"
+	//Modules "github.com/LuisAcerv/goeth-api/modules"
+	//"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gorilla/mux"
+)
+
+// ClientHandler ethereum client instance
+type ClientHandler struct {
+	//*ethclient.Client
+}
+
+func (client ClientHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Get parameter from url request
+	vars := mux.Vars(r)
+	module := vars["module"]
+
+	// Get the query parameters from url request
+	//address := r.URL.Query().Get("address")
+	//hash := r.URL.Query().Get("hash")
+
+	// Set our response header
+	w.Header().Set("Content-Type", "application/json")
+
+	// Handle each request using the module parameter:
+	switch module {
+	case "hello":
+		fmt.Println("hello world")
+
+	case "latest-block":
+		fmt.Println("latest-block")
+		//_block := Modules.GetLatestBlock(*client.Client)
+		//json.NewEncoder(w).Encode(_block)
+
+	case "get-tx":
+		fmt.Println("get-tx")
+		//if hash == "" {
+		//	json.NewEncoder(w).Encode(&Models.Error{
+		//		Code:    400,
+		//		Message: "Malformed request",
+		//	})
+		//	return
+		//}
+		//txHash := common.HexToHash(hash)
+		//_tx := Modules.GetTxByHash(*client.Client, txHash)
+		//
+		//if _tx != nil {
+		//	json.NewEncoder(w).Encode(_tx)
+		//	return
+		//}
+		//
+		//json.NewEncoder(w).Encode(&Models.Error{
+		//	Code:    404,
+		//	Message: "Tx Not Found!",
+		//})
+
+	case "send-eth":
+		fmt.Println("send-eth")
+		//decoder := json.NewDecoder(r.Body)
+		//var t Models.TransferEthRequest
+		//
+		//err := decoder.Decode(&t)
+		//
+		//if err != nil {
+		//	fmt.Println(err)
+		//	json.NewEncoder(w).Encode(&Models.Error{
+		//		Code:    400,
+		//		Message: "Malformed request",
+		//	})
+		//	return
+		//}
+		//_hash, err := Modules.TransferEth(*client.Client, t.PrivKey, t.To, t.Amount)
+		//
+		//if err != nil {
+		//	fmt.Println(err)
+		//	json.NewEncoder(w).Encode(&Models.Error{
+		//		Code:    500,
+		//		Message: "Internal server error",
+		//	})
+		//	return
+		//}
+		//
+		//json.NewEncoder(w).Encode(&Models.HashResponse{
+		//	Hash: _hash,
+		//})
+
+	case "get-balance":
+		fmt.Println("get-balance")
+		//if address == "" {
+		//	json.NewEncoder(w).Encode(&Models.Error{
+		//		Code:    400,
+		//		Message: "Malformed request",
+		//	})
+		//	return
+		//}
+		//
+		//balance, err := Modules.GetAddressBalance(*client.Client, address)
+		//
+		//if err != nil {
+		//	fmt.Println(err)
+		//	json.NewEncoder(w).Encode(&Models.Error{
+		//		Code:    500,
+		//		Message: "Internal server error",
+		//	})
+		//	return
+		//}
+		//
+		//json.NewEncoder(w).Encode(&Models.BalanceResponse{
+		//	Address: address,
+		//	Balance: balance,
+		//	Symbol:  "Ether",
+		//	Units:   "Wei",
+		//})
+
+	}
+
+}
